ffc38b240a6b729b1da9558220ee0bb5ea06268b
tymiurge <tmrlabas@gmail.com>
2018-02-09 23:22:21 +0200

    bootstraping semantic in the project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react'
+import 'semantic-ui-css/semantic.min.css'
+import { Button } from 'semantic-ui-react'
 
 export default class extends Component {
   render() {
     return <div>
       <h2>Welcome to React components</h2>
+      <Button>OK</Button>
     </div>
   }
 }
