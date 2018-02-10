2d3b9af28661cd7643ae35f476d07c7bc996e4b2
tymiurge <tmrlabas@gmail.com>
2018-02-10 21:53:06 +0200

    creating prototype of tags input component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React, {Component} from 'react'
 import 'semantic-ui-css/semantic.min.css'
 import { Button } from 'semantic-ui-react'
+import TagsInput from './tags-input'
 
 export default class extends Component {
   
   render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-      <Button>OK</Button>
-    </div>
+    return <TagsInput tags={['eng', 'fr']}/>
   }
 }
 
--- src/tags-input.js
+++ src/tags-input.js
@@ -1,12 +1,58 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Input, Label, Form, Icon } from 'semantic-ui-react'
 
 export default class extends React.Component {
+
+    static propTypes = {
+        tags: PropTypes.array.isRequired,    
+    }
+
+    state = {
+        inputValue: '',
+        tags: this.props.tags
+    }
+
+    updateInputValue (e) {
+        const newValue = e.target.value
+        this.setState(Object.assign(
+          {}, this.state, {inputValue: newValue}
+        ))
+    }
+
+    catchTagSubmit = (e) => {
+        
+    }
+
     render () {
         return (
-            <React.Fragment>
-                A
-            </React.Fragment>
+            <Form>
+                <Form.Group>
+                    {
+                        this.state.tags.map((tag, idx) => {
+                            return (
+                                <Form.Field key={idx}>
+                                    <Label size="small">
+                                        {tag}
+                                        <Icon 
+                                            name="delete" 
+                                            link 
+                                            // onClick={this.deleteTag.bind(this, tag)}
+                                        />
+                                  </Label>
+                                </Form.Field>
+                            )
+                        })
+                    }
+                    <Form.Field>
+                        <Input transparent placeholder={'props.placeholder'}
+                            value={this.state.inputValue}
+                            onChange={this.updateInputValue.bind(this)}
+                            onKeyPress={this.catchTagSubmit}
+                        />
+                    </Form.Field>
+                </Form.Group>
+            </Form>
         )
     }
 }
