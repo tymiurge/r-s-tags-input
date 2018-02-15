e26121346ee83a4867e362d738bc94e770e7a948
tymiurge <tmrlabas@gmail.com>
2018-02-15 21:20:13 +0200

    TEST: adding tests on Tag component

--- __tests__/tag-test.js
+++ __tests__/tag-test.js
@@ -5,12 +5,33 @@ import Tag from './../src/tag'
 
 configure({ adapter: new Adapter() });
 
-// https://github.com/how-to-react/nwb-jest
+const elms = {
+    tagByText: text => '[text="' + text + '"]',
+    removeIcon: () => 'Icon[name="delete"]'
+}
 
 test('tag with specific text is rendered inside the Tag component', () => {
-    const wrapper = shallow(<Tag text='tag1' />);
     expect(
-        wrapper.find('[text="tag1"]').length
+        shallow(<Tag text='tag1' />).find(elms.tagByText('tag1')).length
     ).toBe(1)
-    
-});
+})
+
+test('tag has remove button if removable = true', () => {
+    expect(
+        shallow(<Tag text='tag1' removable />).find(elms.removeIcon()).length
+    ).toBe(1)
+})
+
+test('tag has NO remove button if removable not defined', () => {
+    expect(
+        shallow(<Tag text='tag1' />).find(elms.removeIcon()).length
+    ).toBe(0)
+})
+
+test('onRemoveClick callbeck is called by clicking on remove button', () => {
+    const onRemoveClickMock = jest.fn();
+    shallow(<Tag text='tag1' removable onRemoveClick={onRemoveClickMock} />)
+        .find(elms.removeIcon())
+        .simulate('click')
+    expect(onRemoveClickMock.mock.calls.length).toBe(1)
+})
